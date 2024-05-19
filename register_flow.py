10313ac795d1afc465590290af8506eeb818bbe6
euneun <145077027+euneuninsep@users.noreply.github.com>
2024-05-19 21:10:42 +0900

    Update register_flow.py

--- register_flow.py
+++ register_flow.py
@@ -1,9 +1,13 @@
 import os
-from prefect import Client
-from stock_flow import flow
+from prefect import flow
+from prefect.deployments import Deployment
+from stock_flow import flow as stock_flow
 
 PREFECT_API_URL = os.environ.get('PREFECT_API_URL')
 
-client = Client(api=PREFECT_API_URL)
+deployment = Deployment.build_from_flow(
+    flow=stock_flow,
+    name="stock-flow-deployment"
+)
 
-flow.register(project_name='StockDataProject')
+deployment.apply()

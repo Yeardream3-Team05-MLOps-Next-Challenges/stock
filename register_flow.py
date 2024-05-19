260b6953804dfc5f13cbd361aed7db96f00139ef
euneun <145077027+euneuninsep@users.noreply.github.com>
2024-05-19 21:52:54 +0900

    Update register_flow.py

--- register_flow.py
+++ register_flow.py
@@ -1,13 +1,14 @@
 import os
 from prefect.deployments import Deployment
-from stock import flow as stock_flow  
+from stock import fetch_and_send_stock_data  
 
 PREFECT_API_URL = os.environ.get('PREFECT_API_URL')
 
 
+
 deployment = Deployment.build_from_flow(
-    flow=stock_flow,
-    name="stock-flow-deployment"
+    flow=fetch_and_send_stock_data,
+    name="fetch-and-send-stock-data-deployment"  
 )
 
 deployment.apply()

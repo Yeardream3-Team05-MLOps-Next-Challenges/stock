9840e072e94ba9d3a7c1d30ed48fa7d103f283cb
euneun <145077027+euneuninsep@users.noreply.github.com>
2024-05-19 21:30:08 +0900

    Update register_flow.py

--- register_flow.py
+++ register_flow.py
@@ -1,9 +1,9 @@
 import os
-from prefect import flow
 from prefect.deployments import Deployment
-from stock_flow import flow as stock_flow
+from stock import flow as stock_flow  
 
 PREFECT_API_URL = os.environ.get('PREFECT_API_URL')
+
 
 deployment = Deployment.build_from_flow(
     flow=stock_flow,

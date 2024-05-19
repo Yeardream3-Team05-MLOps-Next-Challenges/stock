35d293dc826608f36a33ac0eedf6fe42ebeec09c
euneun <145077027+euneuninsep@users.noreply.github.com>
2024-05-20 01:54:25 +0900

    Update register_flow.py

--- register_flow.py
+++ register_flow.py
@@ -1,13 +1,17 @@
 import os
+from dotenv import load_dotenv  # 추가
 from prefect.deployments import Deployment
 from stock import fetch_and_send_stock_data
+
+# .env 파일 로드
+load_dotenv()  # 추가
 
 PREFECT_API_URL = os.getenv('PREFECT_API_URL')
 
 if not PREFECT_API_URL:
     raise ValueError("PREFECT_API_URL 환경 변수가 설정되지 않았습니다.")
-
-print("PREFECT_API_URL is set")  # 환경 변수가 설정되었음을 확인
+else:
+    print("PREFECT_API_URL is set")
 
 deployment = Deployment.build_from_flow(
     flow=fetch_and_send_stock_data,

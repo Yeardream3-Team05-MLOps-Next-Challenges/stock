37fe2f32fe2a09648acd68e843d6e8af66d417b9
euneun <145077027+euneuninsep@users.noreply.github.com>
2024-05-19 21:01:17 +0900

    Update register_flow.py

--- register_flow.py
+++ register_flow.py
@@ -1,9 +1,9 @@
 import os
-from prefect.client.orion import OrionClient
+from prefect import Client
 from stock_flow import flow
 
 PREFECT_API_URL = os.environ.get('PREFECT_API_URL')
 
+client = Client(api=PREFECT_API_URL)
 
-client = OrionClient(api_url=PREFECT_API_URL)
 flow.register(project_name='StockDataProject')

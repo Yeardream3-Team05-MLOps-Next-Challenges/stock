e48bdfed58a3d04772e0fe2b92f8d79fa7ca6b84
euneun <145077027+euneuninsep@users.noreply.github.com>
2024-05-20 00:42:47 +0900

    Update register_flow.py

--- register_flow.py
+++ register_flow.py
@@ -4,7 +4,10 @@ from stock import fetch_and_send_stock_data
 
 PREFECT_API_URL = os.environ.get('PREFECT_API_URL')
 
+if not PREFECT_API_URL:
+    raise ValueError("PREFECT_API_URL 환경 변수가 설정되지 않았습니다.")
 
+print("PREFECT_API_URL is set")  # 환경 변수가 설정되었음을 확인
 
 deployment = Deployment.build_from_flow(
     flow=fetch_and_send_stock_data,
@@ -13,4 +16,4 @@ deployment = Deployment.build_from_flow(
 
 deployment.apply()
 
-
+print("Deployment created and applied successfully.")

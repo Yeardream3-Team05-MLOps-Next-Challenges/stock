361b549695aefcfd7f37e2cf1bc3266cefa0b7e0
euneun <145077027+euneuninsep@users.noreply.github.com>
2024-06-11 21:31:51 +0900

    Update register_flow.py

--- register_flow.py
+++ register_flow.py
@@ -1,10 +1,12 @@
 import os
-from dotenv import load_dotenv  # 추가
+from dotenv import load_dotenv
 from prefect.deployments import Deployment
+from prefect.infrastructure.docker import DockerContainer
+from prefect.agent.docker import DockerAgent
 from stock import fetch_and_send_stock_data
 
 # .env 파일 로드
-load_dotenv()  # 추가
+load_dotenv()
 
 PREFECT_API_URL = os.getenv('PREFECT_API_URL')
 
@@ -13,11 +15,32 @@ if not PREFECT_API_URL:
 else:
     print("PREFECT_API_URL is set")
 
+# Docker 컨테이너 설정
+docker_block = DockerContainer(
+    image="docker_image_stock:tag",
+    image_pull_policy="ALWAYS",
+    auto_remove=True
+)
+
+docker_block.save("stock-data-container", overwrite=True)
+
+# Flow와 Deployment 정의 및 적용
 deployment = Deployment.build_from_flow(
     flow=fetch_and_send_stock_data,
-    name="fetch-and-send-stock-data-deployment"
+    name="fetch-and-send-stock-data-deployment",
+    infrastructure=docker_block,
+    tags=["stock", "data"]
 )
 
 deployment.apply()
 
 print("Deployment created and applied successfully.")
+
+# 에이전트 설정 및 실행
+agent = DockerAgent(
+    name="stock-data-agent",
+    work_queue="default",
+    show_flow_logs=True
+)
+
+agent.start()
